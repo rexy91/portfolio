06370879bf9069e85b77eb2c0a157f586d93f2bd
Rex Ye <rxye1991@gmail.com>
2020-03-22 16:25:31 -0400

    testing

--- src/components/contact.js
+++ src/components/contact.js
@@ -18,20 +18,26 @@ export class contact extends Component {
       // </form>
       // </div>
       <div>
-            <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-            <p class="hidden">
-            <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-            </p>
-            <p>
-            <label>Email: <input type="text" name="email" /></label>
-            </p>
-            <p>
-            <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-            <button type="submit">Send</button>
-            </p>
-            </form>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
       </div>
     )
   }
