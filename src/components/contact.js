7cb1de5d1523a26ac54c3f92565088902170d550
Rex Ye <rxye1991@gmail.com>
2020-03-22 16:10:09 -0400

    netlifyFormsubmissin

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,14 +5,15 @@ export class contact extends Component {
     return (
       
       <div id='contactformdiv'>
-      <form id ='contactForm'>
+      <form id ='contactForm' method = "POST" data-netlify = "true">
       <h3>Get In Touch</h3>
       <label for="fname">Name</label>
       <input type="text" id="fullname" name="fullname" placeholder="Your name.."/>
       <label for='email'>Email </label>
-      <input type='text' id='email' placeholder='Your email'/>
+      <input type='email' name='email' id='email' placeholder='Your email'/>
       <label for='message'>Message</label>
       <textarea id="message" name="message" placeholder='your message ' style= {{height:'200px'}}></textarea>
+      <div data-netlify-recaptcha="true"></div>
       <input type='submit' value='Send Now'/>
       </form>
       </div>
