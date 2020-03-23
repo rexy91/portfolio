11b1c006d824e693ac5d7b66fa514ca83cec423e
Rex Ye <rxye1991@gmail.com>
2020-03-23 13:17:21 -0400

    contactFormSubmissionDone

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,36 +4,21 @@ export class contact extends Component {
   render() {
     return (
       
-      // <div id='contactformdiv'>
-      // <form id ='contactForm' method = "POST" data-netlify = "true">
-      // <h3>Get In Touch</h3>
-      // <label for="fname">Name</label>
-      // <input type="text" id="fullname" name="fullname" placeholder="Your name.."/>
-      // <label for='email'>Email </label>
-      // <input type='email' name='email' id='email' placeholder='Your email'/>
-      // <label for='message'>Message</label>
-      // <textarea id="message" name="message" placeholder='your message ' style= {{height:'200px'}}></textarea>
-      // <div data-netlify-recaptcha="true"></div>
-      // <input type='submit' value='Send Now'/>
-      // </form>
-      // </div>
-      <div>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
+      <div id='contactformdiv'>
+      <form id ='contactForm' method = "POST" data-netlify = "true">
+      <input type="hidden" name="form-name" value="contact" />
+      <h3>Get In Touch</h3>
+      <label for="fname">Name</label>
+      <input type="text" id="fullname" name="name" placeholder="Your name.."/>
+      <label for='email'>Email </label>
+      <input type='email' name='email' id='email' placeholder='Your email'/>
+      <label for='message'>Message</label>
+      <textarea id="message" name="message" placeholder='your message ' style= {{height:'200px'}}></textarea>
+      <div data-netlify-recaptcha="true"></div>
+      <input type='submit' value='Send Now'/>
+      </form>
+	  </div>
+
     )
   }
 }
