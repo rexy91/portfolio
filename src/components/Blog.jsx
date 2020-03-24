971092533b884cbce43f878e89cbaaf0473fdea2
Rex Ye <rxye1991@gmail.com>
2020-03-24 15:17:03 -0400

    blogPageFinished

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,31 +7,46 @@ import binarysearch from './blogphotos/binarysearch.jpg'
 import {withRouter} from 'react-router-dom'
 
 export class Blog extends Component {
-
-    openBlog = () => {
-        
+    openBlog = (e) => {
+        const activeRecordUrl = 'https://medium.com/@rxye91/activerecord-associations-36e049e733bb'
+        const callbackFunctionUrl = 'https://blog.usejournal.com/callback-functions-in-javascript-b3a622586556'
+        const bootstrapUrl = 'https://medium.com/@rxye91/bootstrap-grid-system-12c9f4d96558'
+        const binarysearchUrl = 'https://levelup.gitconnected.com/binary-search-algorithm-in-javascript-5227bba08e62'
+        if (e.target.name ==='activeRecord'){
+            window.open(`${activeRecordUrl}`)
+        }
+        else if (e.target.name ==='callbackFunction'){
+            window.open(`${callbackFunctionUrl}`)
+        }
+        else if (e.target.name ==='gridSystem'){
+            window.open(`${bootstrapUrl}`)
+        }
+        else if (e.target.name === 'binarySearch') {
+            window.open(`${binarysearchUrl}`)
+        }
     }
+    
     render() {
         return (
             <div id = 'blogPage'>
                 <div className = 'ui stackable two column grid'>
 
                     <div className = 'eight wide column blogContainer' >
-                                <h4 onClick = {this.openBlog} name = 'activeRecord'style={{marginLeft:'10vw'}}>Active Record Associations</h4>
+                                <h5 onClick = {this.openBlog} name = 'activeRecord'style={{marginLeft:'10vw'}}>Active Record Associations</h5>
                                 <img onClick = {this.openBlog} name = 'activeRecord' style={{marginLeft:'10vw'}}src={railsblog} alt=""/>                        
                     </div>
                     <div className = 'eight wide column blogContainer' >
-                                  <h4 style={{marginRight:'10vw'}}>JavaScript Callback Functions</h4>
-                                  <img style={{marginRight:'10vw', marginTop:'1vh'}} src={callbackfunction} alt=""/>            
+                                  <h5 name = 'callbackFunction' onClick = {this.openBlog} style={{marginRight:'10vw'}}>JavaScript Callback Functions</h5>
+                                  <img name = 'callbackFunction' onClick = {this.openBlog} style={{marginRight:'10vw', marginTop:'1vh'}} src={callbackfunction} alt=""/>            
                     </div>
                     <div className = 'eight wide column blogContainer'>
-                                <h4 style={{marginLeft:'10vw'}}>Bootstrap Grid System</h4>
-                                <img style={{marginLeft:'10vw'}} src={bootstrap} alt=""/> 
+                                <h5 name = 'gridSystem'  onClick = {this.openBlog} style={{marginLeft:'10vw'}}>Bootstrap Grid System</h5>
+                                <img name = 'gridSystem' onClick = {this.openBlog} style={{marginLeft:'10vw'}} src={bootstrap} alt=""/> 
                     </div>
 
                     <div className = 'eight wide column blogContainer'>
-                                <h4  style={{marginRight:'10vw'}}> Binary Search Algorithm</h4>
-                                <img  style={{marginRight:'10vw'}} src={binarysearch} alt=""/> 
+                                <h5  name = 'binarySearch' onClick = {this.openBlog} style={{marginRight:'10vw'}}> Binary Search Algorithm</h5>
+                                <img name = 'binarySearch' onClick = {this.openBlog} style={{marginRight:'10vw'}} src={binarysearch} alt=""/> 
                     </div>              
                 </div>
                 {/* <div class="ui grid">
