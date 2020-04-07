7bc69bdc2efbe46dbd2cf9f8d8ac535fa4b4a105
Rex Ye <rxye1991@gmail.com>
2020-04-06 21:02:04 -0400

    changedJobCompaniesFontsizes

--- src/components/experience.js
+++ src/components/experience.js
@@ -9,8 +9,8 @@ class Experience extends Component {
           <p style = {{fontSize:'16px'}}>{this.props.startYear} - {this.props.endYear}</p>
         </Cell>
         <Cell col={8}>
-          <h4 style={{marginTop:'0px'}}>{this.props.jobName}</h4>
-          <h5>{this.props.position}</h5>
+          <h5 style={{marginTop:'0px'}}>{this.props.jobName}</h5>
+          <p>{this.props.position}</p>
           <li>{this.props.descriptionLine1}</li>
           <li>{this.props.descriptionLine2}</li>
           {this.props.descriptionLine3 ? <li>{this.props.descriptionLine3}</li> : null}
