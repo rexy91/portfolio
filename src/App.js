50589e5d5e6053c82a9a8246d1d9d394c4b5630b
Rex Ye <rxye1991@gmail.com>
2020-09-03 17:29:34 -0400

    fixedMarginofnavbaritems

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import { Link } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Main from './components/main'
-
-// Components
-
-import Resume from'./components/resume'
-import Projects from './components/projects'
 
 class App extends Component {
   render() {
