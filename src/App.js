d26e930ea20ff7a1183cdcee9cf031b0f258e684
Rex Ye <rxye1991@gmail.com>
2020-09-03 13:28:25 -0400

    ff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import LandingRedo from './components/LandingRedo/LandingRedo'
 import { Link } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
+
+// Components
+import LandingRedo from './components/LandingRedo/LandingRedo'
+import Resume from'./components/resume'
+import Projects from './components/projects'
+
 class App extends Component {
   render() {
     return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,13 +21,14 @@ class Navbar extends Component {
                         {/* Burger on and off  */}
                         <i className={this.state.clicked? 'fas fa-times' : 'fas fa-bars'}> </i>
                     </div>
-                    
+
                     <ul className={this.state.clicked? 'nav-menu active' : 'nav-menu'}>
-                        <li><a className='nav-links' href='#'>About</a></li>
-                        <li><a className='nav-links' href='#'>Resume</a></li>
-                        <li><a className='nav-links' href='#'>Projects</a></li>
-                        <li><a className='nav-links' href='#'>Blogs</a></li>
-                        <li><a className='nav-links' href='#'>Contact</a></li>
+                        {/* To close the side bar menu on moble, all you gotta do is just change the state. */}
+                        <li><a className='nav-links' href='#' onClick={this.handleClick}>About</a></li>
+                        <li><a className='nav-links' href='#' onClick={this.handleClick}>Resume</a></li>
+                        <li><a className='nav-links' href='#' onClick={this.handleClick}>Projects</a></li>
+                        <li><a className='nav-links' href='#' onClick={this.handleClick}>Blogs</a></li>
+                        <li><a className='nav-links' href='#' onClick={this.handleClick}>Contact</a></li>
                     </ul>
             </nav>           
         )
